ab39686d99930dd380ded4cd2907a5606388225a
unknown <mdshahreerirfan@gmail.com>
2024-01-05 02:09:14 +0600

    Recap

--- Project-14/Banglar-T-Shirt/src/Components/Aunty/Aunty.jsx
+++ Project-14/Banglar-T-Shirt/src/Components/Aunty/Aunty.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Aunty = () => {
     return (
         <div>
-            
+            <h2>Aunty</h2>
         </div>
     );
 };
--- Project-14/Banglar-T-Shirt/src/Components/Grandpa/Grandpa.jsx
+++ Project-14/Banglar-T-Shirt/src/Components/Grandpa/Grandpa.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Father from '../Father/Father';
+import Uncle from '../Uncle/Uncle';
+import Aunty from '../Aunty/Aunty';
+import './Grandpa.css'
 
 const Grandpa = () => {
     return (
-        <div>
+        <div className='grandpa'>
             <h3>GrandPa</h3>
+            <section className='flex'>
+                <Father></Father>
+                <Uncle></Uncle>
+                <Aunty></Aunty>
+            </section>
         </div>
     );
 };
