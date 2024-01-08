aff7eeb231523eb00ebe8580e3bb866c4cef19a0
unknown <mdshahreerirfan@gmail.com>
2024-01-08 20:48:11 +0600

    validation

--- Project-18/email-password-auth/src/Components/Login/Login.jsx
+++ Project-18/email-password-auth/src/Components/Login/Login.jsx
@@ -1,10 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [error,setError] = useState('');
+    const [success,setSuccess] = useState('');
+
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        // Validation
+        setError('')
+        setSuccess('')
+        if(!/^[A-Z]{2}$/.test(password)){
+            setError('Enter at least 2 upper case')
+            return;
+        }
+        else if(!/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}/[\]:;<>,.?~\\/-]).+$/.test(password)){
+            setError('Add a special character')
+            return;
+        }
+    };
+
     return (
         <div>
             <h2>This is login</h2>
+            <div>
+                <form onSubmit={handleLogin}>
+                    <div className="form-group">
+                        <input required name='email' type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input required name='password' type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <br />
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </form>
+                <p className='text-warning'>{error}</p>
+            </div>
         </div>
     );
 };
--- Project-18/email-password-auth/src/Components/Rerister/Register.jsx
+++ Project-18/email-password-auth/src/Components/Rerister/Register.jsx
@@ -6,8 +6,12 @@ import app from '../../FireBase/FireBase.Config';
 const auth = getAuth(app);
 
 const Register = () => {
+    const [error,setError] = useState('');
     const [email, SetEmail] = useState('');
     const [password, setPassword] = useState('')
+    const [success,setSuccess] = useState('')
+
+
 
     const handleEmailChange = (event) => {
         // console.log(event.target.value)
@@ -20,13 +24,18 @@ const Register = () => {
 
 
     const handleSubmit = (event) => {
+        
         event.preventDefault();
+        setError('')
         const email = event.target.email.value;
         const password = event.target.password.value;
-    
-        // Check if the password meets the minimum length requirement
-        if (password.length < 6) {
-            console.error("Password should be at least 6 characters");
+        // Validition cheking
+        if(!/(?=.*[A-Z])/.test(password)){
+            setError('Atleast one upeer case')
+            return;
+        }
+        else if (password.length < 6) {
+            setError('Password must be 6 character')
             return;
         }
     
@@ -34,9 +43,14 @@ const Register = () => {
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
+                setError('')
+                event.target.reset()
+                setSuccess('User created successfully');
             })
             .catch(error => {
-                console.error(error);
+                console.error(error.message);
+                setError(error.message)
+                setSuccess('')
             });
     }
 
@@ -44,19 +58,20 @@ const Register = () => {
         <div className='form-group'>
             <h2>This is register</h2>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Enter your E-mail' />
+                <input required onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Enter your E-mail' />
                 <br />
                 <br />
-                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Enter your password' />
+                <input required onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Enter your password' />
                 <br />
                 <br />
+                
                 <input className='btn btn-primary form-control' type="submit" value="Register" />
             </form>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
+            
         </div>
     );
 };
 
 export default Register;
-
-
-// 
