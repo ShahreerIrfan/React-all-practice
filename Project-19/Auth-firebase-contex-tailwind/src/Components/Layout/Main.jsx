b3be82b31206dc8d6a76247bc372fd60a91606f2
unknown <mdshahreerirfan@gmail.com>
2024-01-09 19:07:10 +0600

    new

--- Project-19/Auth-firebase-contex-tailwind/src/Components/Layout/Main.jsx
+++ Project-19/Auth-firebase-contex-tailwind/src/Components/Layout/Main.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Home from '../Home/Home';
+import Header from '../Header/Header';
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Header></Header>
+            <Home></Home>
         </div>
     );
 };
---
+++ Project-19/Auth-firebase-contex-tailwind/src/Components/Register/Register.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>This is register</h3>
+        </div>
+    );
+};
+
+export default Register;
