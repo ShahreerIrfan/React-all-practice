cfe584b423ba4f950ae3ce742bcb75631ac72fe8
unknown <mdshahreerirfan@gmail.com>
2024-01-11 18:56:58 +0600

    ema jhon

--- Project-20/Ema-Jhon/src/assets/Components/Login/Login.jsx
+++ Project-20/Ema-Jhon/src/assets/Components/Login/Login.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h3>This is login page</h3>
+        <div className='form-container'>
+            <h3 className='form-title'>This is login page</h3>
+            <form action="">
+                <div className="form-contron">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name='email' required />
+                </div>
+            </form>
         </div>
     );
 };
