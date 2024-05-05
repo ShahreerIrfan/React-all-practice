db305037b5497b65e66b9b404cdae6b443d3f36f
Your Name <mdshahreerirfan@gmail.com>
2024-05-05 20:32:38 +0600

    [Added] HTML Started batch 8

---
+++ Batch-08/Project-01/react-world-tour/src/components/Countries/Countries.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
